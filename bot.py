847d6785026b474ad56aa6702e0c57b60cb5d5dd
user <rudich.official@gmai.com>
2021-04-19 13:56:10 +0300

    BNB AND XRP BUTTON ON TELEGRAM

--- bot.py
+++ bot.py
@@ -3,19 +3,37 @@ The main app logic.
 """
 
 import telebot
+from telebot import types
 import config
-from cryptoParser import bnb_value
+from cryptoParser import bnb_value, xrp_value
 
 bot = telebot.TeleBot(config.TOKEN)
 
 print(bnb_value())
+print(xrp_value())
 
 
-@bot.message_handler(commands=['bnb', 'start'])
-def send_welcome(message):
+@bot.message_handler(content_types=['text'], commands=['start'])
+def send_bnb(message):
+    keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    button_bnb = types.KeyboardButton(text='bnb')
+    button_xrp = types.KeyboardButton(text='xrp')
+    keyboard.add(button_bnb, button_xrp)
+    bot.send_message(message.chat.id,
+                     "Добро пожаловать в CryptoAlarmBot, в данном боте можно отслеживать курс актуальных криптовалют "
+                     "и получать уведомления о коррекции/повышении/понижении курса. Выберете криптовалюту.",
+                     reply_markup=keyboard)
 
-    msg = bot.send_message(message.chat.id, f'Курс bnb на текущий момент {bnb_value()}')
 
+@bot.message_handler(content_types=['text'])
+def send_bnb(message):
+    get_message_bot = message.text.strip().lower()
+    if get_message_bot == "xrp":
+        xrp_price = bot.send_message(message.chat.id, f'Курс xrp на текущий момент {xrp_value()}$')
+        bot.delete_message(message.chat.id, message.message_id)
+    elif get_message_bot == "bnb":
+        bnb_price = bot.send_message(message.chat.id, f'Курс bnb на текущий момент {bnb_value()}$')
+        bot.delete_message(message.chat.id, message.message_id)
 
 
 @bot.message_handler(commands=['auth'])
--- cryptoParser.py
+++ cryptoParser.py
@@ -5,14 +5,24 @@ Version: 0.1
 Developer: Rudich ILya
 """
 import time
-
 import requests
 from bs4 import BeautifulSoup
 
+XRP_DOLLAR = 'https://coinmarketcap.com/currencies/xrp/'
 BNB_DOLLAR = 'https://www.coingecko.com/en/coins/binance-coin'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) '
                   'Chrome/89.0.4389.128 Safari/537.36'}
+
+
+def xrp_value():
+    full_page = requests.get(XRP_DOLLAR, headers)
+    soup = BeautifulSoup(full_page.content, 'html.parser')
+    convert = soup.find("div", {"class": "priceValue___11gHJ"})
+    convert = str(convert)
+    convert = convert[33:37]
+    ripple = convert
+    return ripple
 
 
 def bnb_value():
@@ -20,6 +30,6 @@ def bnb_value():
     soup = BeautifulSoup(full_page.content, 'html.parser')
     convert = soup.find("span", {"data-target": "price.price"})
     convert = str(convert)
-    convert = convert[127:133]
+    convert = convert[128:133]
     bnb = convert
     return bnb
