707f0ec3b26479723ba3c4ab7170304c0a6dd4fe
bigel <rudich.official@gmail.com>
2021-04-19 08:53:25 +0300

    v3

--- bot.py
+++ bot.py
@@ -1,19 +1,26 @@
-
 """
 The main app logic.
 """
 
 import telebot
 import config
+from cryptoParser import bnb_value
 
-bot = telebot.TeleBot(config.TOKEN, parse_mode=None)
+bot = telebot.TeleBot(config.TOKEN)
 
-
-@bot.message_handler(content_types=['text'])
-def echo_all(message):
-    bot.reply_to(message, message.text)
+print(bnb_value())
 
 
-# Run
+@bot.message_handler(commands=['bnb', 'start'])
+def send_welcome(message):
 
-bot.polling(none_stop=True)
+    msg = bot.send_message(message.chat.id, f'Курс bnb на текущий момент {bnb_value()}')
+
+
+
+@bot.message_handler(commands=['auth'])
+def send_auth(message):
+    pass
+
+
+bot.polling()
